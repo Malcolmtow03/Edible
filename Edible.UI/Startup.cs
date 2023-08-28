cf4b1d2116b93cd0d9285d0d706876377cf5dc83
Malcolmtow03 <137528460+Malcolmtow03@users.noreply.github.com>
2023-08-28 23:05:47 +0200

    Add project files.

---
+++ Edible.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Edible.UI.Startup))]
+namespace Edible.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
