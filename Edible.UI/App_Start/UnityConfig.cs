e85e8b798b8e7e7d35df23a8deac958dc1f40be5
Malcolmtow03 <137528460+Malcolmtow03@users.noreply.github.com>
2023-09-07 20:19:13 +0200

    Updates

--- Edible.UI/App_Start/UnityConfig.cs
+++ Edible.UI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using Edible.Core.Contracts;
 using Edible.Core.Models;
 using Edible.DataAccess.Inmemory;
+using Edible.DataAccess.Sql;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace Edible.UI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
 
 
         }
